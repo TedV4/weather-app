db29ba0d4cef76270f9e5225a430c534089a9975
TedV4 <tedverdonkschot@outlook.com>
2023-01-30 13:35:17 -0700

    Update DOM elements with weather data

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
-const searchBar = document.getElementById('searchBar');
-let weather = { location: "", temperature: "", humidity: "", wind: "" };
+// let weather = { location: "why no", temperature: "", humidity: "", wind: "" };
+const locationForm = document.getElementById("locationForm");
+const locationName = document.getElementById("locationName");
+const temp = document.getElementById("temp");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
 
 async function getWeather(location) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=03c0b915c9fbaaf599e9d894ffbf6cf1&units=imperial`,
-    { mode: "cors" }
-  );
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=03c0b915c9fbaaf599e9d894ffbf6cf1&units=imperial`,
+      { mode: "cors" }
+    );
 
-  const weatherData = await response.json();
-  weather.location = weatherData.name;
-  weather.temperature = `${weatherData.main.temp}F°`;
-  weather.humidity = `${weatherData.main.humidity}%`;
-  weather.wind = `${weatherData.wind.speed}mph`;
+    const weatherData = await response.json();
 
-  console.log(weather);
-  console.log(weatherData);
+    locationName.textContent = weatherData.name;
+    temp.textContent = `Temperature: ${weatherData.main.temp}F°`;
+    humidity.textContent = `Humidity: ${weatherData.main.humidity}%`;
+    wind.textContent = `Wind: ${weatherData.wind.speed}mph`;
+  } catch (err) {
+    locationName.textContent = "Location not found.";
+    temp.textContent = "";
+    humidity.textContent = "";
+    wind.textContent = "";
+  }
 }
 
-getWeather('London');
+locationForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  getWeather(searchBar.value);
+  locationForm.reset();
+});
+
+getWeather("London");
